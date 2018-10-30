b9a1203cc4372f06f37d6660d58e163b7b155656
Carlos dos Santos <cds@cds-software.com.br>
2018-10-30 15:33:53 -0300

    serialize

---
+++ FileSerializeUnSerialize/FileSerializeUnSerialize/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSerializeUnSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from file
+            var fsRead = new FileStream("LogoCDS.png", FileMode.Open);
+            int tamanho = (int)fsRead.Length;
+            byte[] buffer = new byte[tamanho];
+            fsRead.Read(buffer, 0, tamanho);
+
+            // create a temp file name and change extension
+            var tempFile = Path.GetFileName(Path.GetTempFileName());
+            tempFile = Path.ChangeExtension(tempFile, "png");
+
+            // write serialized file
+            var fsWrite = new FileStream(tempFile, FileMode.Create);
+            fsWrite.Write(buffer, 0, tamanho);
+            fsWrite.Close();
+        }
+    }
+}
