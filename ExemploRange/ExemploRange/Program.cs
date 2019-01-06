0fe09896e9f13b993060c045dcd1357c88467df4
Carlos dos Santos <cds@cds-software.com.br>
2019-01-06 21:10:57 -0200

    C# 8 Range Index

---
+++ ExemploRange/ExemploRange/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExemploRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (int i in num[1..5])
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine("");
+
+            string str = "Exemplo em C# 8";
+
+            foreach (var s in str[^4..str.Length])
+            {
+                Console.Write(s);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
