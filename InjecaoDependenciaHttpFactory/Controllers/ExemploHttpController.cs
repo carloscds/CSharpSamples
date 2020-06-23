5f97787d0d1057ecd75b557ffa47a4e2a5b774db
Carlos dos Santos <cds@cds-software.com.br>
2020-06-23 16:34:53 -0300

    Exemplo HttpFactory

---
+++ InjecaoDependenciaHttpFactory/Controllers/ExemploHttpController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Http;
+using System.Net.Http;
+
+namespace InjecaoDependenciaHttpFactory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExemploHttpController : ControllerBase
+    {
+
+        private readonly HttpClient _client;
+        public ExemploHttpController(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string local)
+        {
+            if(string.IsNullOrEmpty(local))
+            {
+               local = "America/Sao_Paulo";
+            }
+            var url = $"http://worldtimeapi.org/api/timezone/{local}";
+            var response = await _client.GetAsync(url);
+            return Ok(await response.Content.ReadAsStringAsync());
+        }
+
+        [HttpGet("locais")]
+        public async Task<IActionResult> GetLocais()
+        {
+            var url = $"http://worldtimeapi.org/api/timezone";
+            var response = await _client.GetAsync(url);
+            return Ok(await response.Content.ReadAsStringAsync());
+        }
+
+
+    }
+}
---
+++ InjecaoDependenciaHttpFactory/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace InjecaoDependenciaHttpFactory
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpClient();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
