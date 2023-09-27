16d1be726704877b9a623471301b88c82ce88cb2
Carlos dos Santos <cds@cds-software.com.br>
2023-09-26 21:53:13 -0300

    New Reflection

---
+++ ReflectionNewWay/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ReflectionNewWay;
+
+public class Benchmarks
+{
+    private readonly Teste _teste = new Teste();
+
+    [Benchmark]
+    public void Get_Method()
+    {
+        NovoReflector.GetMethod(_teste);
+    }
+
+    [Benchmark]
+    public void Get_Field()
+    {
+        NovoReflector.GetField(_teste) = "Novo Field";
+    }
+
+}
---
+++ ReflectionNewWay/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Running;
+using ReflectionNewWay;
+using System.Runtime.CompilerServices;
+
+BenchmarkRunner.Run<Benchmarks>();
+return;
+
+var teste = new Teste();
+var method = NovoReflector.GetMethod(teste);
+NovoReflector.GetField(teste) = "Novo Field";
+Console.WriteLine(teste);
+
+
+public class NovoReflector
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Metodo")]
+    public static extern string GetMethod(Teste teste);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "Campo")]
+    public static extern ref string GetField(Teste teste);
+}
+
+public class Teste
+{
+    private string Campo = "Campo";
+    private string Metodo() => "Carlos";
+}
